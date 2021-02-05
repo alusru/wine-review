a888c2ad0c77df4573b365a952fbfc5031377761
mbusonkabinde <mbuso@sabinet.co.za>
2021-02-05 13:33:03 +0200

    feat(app): added a reactivebase component and datasearch component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {ReactiveBase,DataSearch} from '@appbaseio/reactivesearch';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ReactiveBase app="wine" url="https://SRl6mJD2L:bd3e68cd-06e4-4336-af86-7c9890ddfe67@wine-data-xvryxsf-arc.searchbase.io">
+     <DataSearch componentId="SearchSensor" dataField={['title','title.search','title.autosuggest','variety','variety.search','variety.autosuggest']}
+     onValueSelected={(value,cause, source)=>{
+       console.log('selected value',value);
+       console.log('selected obj',source);
+     } }
+     
+     />
+     </ReactiveBase>
+      
     </div>
   );
 }
